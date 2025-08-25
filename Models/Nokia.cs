58a1e572d6f11ed82991ef2a3901cc23bf46baa6
rhuanlunz <5934.breezy080@passinbox.com>
2025-08-25 14:22:43 -0300

    Fix: Added constructors to Iphone and Nokia classes. Add: added tests

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -2,6 +2,11 @@ namespace DesafioPOO.Models
 {
     public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) 
+            : base(numero, modelo, imei, memoria)
+        { 
+        }
+
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Instalando aplicativo {nomeApp}...");
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Teste da classe Nokia
+var nokia = new Nokia("+55 27 99999-9999", "Nokia 8.3 5G", "987654321012345", 64);
+
+nokia.Ligar();
+
+nokia.ReceberLigacao();
+
+nokia.InstalarAplicativo("Nokia Snake Adventure");
+
+// Teste da classe Iphone
+var iphone = new Iphone("+55 27 97777-7777", "iPhone 13 Pro", "0123456789012345", 256);
+
+iphone.Ligar();
+
+iphone.ReceberLigacao();
+
+iphone.InstalarAplicativo("Bubble Pop Frenzy");
